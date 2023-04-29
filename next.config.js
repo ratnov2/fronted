b6a9fb6d73e3d4875266589811e893ba9c30d0ce
ratnov2 <73044570+ratnov2@users.noreply.github.com>
2023-04-29 16:51:59 +0300

    add images upload

--- next.config.js
+++ next.config.js
@@ -9,12 +9,10 @@ const nextConfig = {
   images: {
     domains: [
       'test2-ratnov2.vercel.app',
-      'downloader.disk.yandex.ru',
-      'cloud-api.yandex.net',
+      'i.postimg.cc',
       'downloader.disk.yandex.ru',
     ],
   },
-  // https://uploader24g.disk.yandex.net/
   async rewrites() {
     return [
       {
@@ -23,45 +21,8 @@ const nextConfig = {
           ? `https://test2-ratnov2.vercel.app/api/:path*`
           : 'http://localhost:4200/api/:path*',
       },
-      {
-        source: '/disk/:path*',
-        destination: `https://cloud-api.yandex.net/v1/disk/:path*`,
-      },
-      {
-        source: '/_next/:path*',
-        destination: `http://localhost:4200/_next/:path*`,
-      },
-      // {
-      //   source: '/image/:path*',
-      //   destination: `path*`,
-      // },
-      // {
-      //   source: '/:path*',
-      //   destination: `/:path*`,
-
-      // },
     ]
   },
-  // async headers() {
-  //   return [
-  //     {
-  //       source: '/disk/',
-  //       headers: [
-  //         { key: 'Access-Control-Allow-Credentials', value: 'true' },
-  //         { key: 'Access-Control-Allow-Origin', value: '*' },
-  //         {
-  //           key: 'Access-Control-Allow-Methods',
-  //           value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-  //         },
-  //         {
-  //           key: 'Access-Control-Allow-Headers',
-  //           value:
-  //             'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-  //         },
-  //       ],
-  //     },
-  //   ]
-  // },
 }
 
 module.exports = nextConfig
