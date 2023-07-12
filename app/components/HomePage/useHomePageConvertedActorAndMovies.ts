3fd7450bc7f4108812a097297433bb63dc9453e0
ratnov2 <73044570+ratnov2@users.noreply.github.com>
2023-07-13 01:12:23 +0300

    vercel v3

--- app/components/HomePage/useHomePageConvertedActorAndMovies.ts
+++ app/components/HomePage/useHomePageConvertedActorAndMovies.ts
@@ -3,20 +3,25 @@ import { useGlobalProps } from 'global-props/contexts/GlobalPropsContext'
 
 export const useHomePageConvertedActorAndMovies = () => {
   const { actors, popularMovies } = useGlobalProps()
-  const popularMoviesConverted = popularMovies.map((el) => ({
-    posterPath: el.poster,
-    name: el.title,
-    url: getMovieUrl(el._id),
-  }))
-  const popularActorsConverted = actors.map((el) => ({
-    posterPath: el.photo,
-    name: el.name,
-    url: getActorUrl(el._id),
-    content:{
-      title:el.name,
-      subTitle:String(el.countMovies)
-    }
-  }))
-  console.log('@##',popularMovies)
+  const popularMoviesConverted =
+    popularMovies !== undefined
+      ? popularMovies.map((el) => ({
+          posterPath: el.poster,
+          name: el.title,
+          url: getMovieUrl(el._id),
+        }))
+      : []
+  const popularActorsConverted =
+    actors !== undefined
+      ? actors.map((el) => ({
+          posterPath: el.photo,
+          name: el.name,
+          url: getActorUrl(el._id),
+          content: {
+            title: el.name,
+            subTitle: String(el.countMovies),
+          },
+        }))
+      : []
   return { popularMoviesConverted, popularActorsConverted }
 }
--- app/ui/slider/useSliderEffect.ts
+++ app/ui/slider/useSliderEffect.ts
@@ -1,17 +1,20 @@
 import { useGlobalProps } from 'global-props/contexts/GlobalPropsContext'
 import { useState } from 'react'
 
-export const  useSliderEffect = () => {
+export const useSliderEffect = () => {
   const [currentIdx, setCurrentIdx] = useState(0)
   const { popularMovies } = useGlobalProps()
   const [slideIn, setSlideIn] = useState(false)
 
-  const popularMoviesConverted = popularMovies.map((el) => ({
-    id: el._id,
-    bigPoster: el.bigPoster,
-    genres: el.genres,
-    title: el.title,
-  }))
+  const popularMoviesConverted =
+    popularMovies !== undefined
+      ? popularMovies.map((el) => ({
+          id: el._id,
+          bigPoster: el.bigPoster,
+          genres: el.genres,
+          title: el.title,
+        }))
+      : []
 
   const nextIndex = () => {
     if (currentIdx === 2) return setCurrentIdx(0)
