12aefa5e8a0737686d8342bf24ad7d2d82e40065
ratnov2 <73044570+ratnov2@users.noreply.github.com>
2023-07-12 18:47:48 +0300

    v2

--- app/global-props/contexts/GlobalPropsContext.tsx
+++ app/global-props/contexts/GlobalPropsContext.tsx
@@ -1,30 +1,56 @@
-import { GlobalProps } from "global-props/GlobalProps";
-import { createContext, useContext, ReactNode } from "react"
+import { GlobalProps } from 'global-props/GlobalProps'
+import { createContext, useContext, ReactNode } from 'react'
 
 // Default value for global props
 export const defaultGlobalPropsContextValue: GlobalProps = {
-    popularMovies: [],
-    actors:[],
+  popularMovies: [
+    {
+      _id: '1',
+      actors: ['12'],
+      bigPoster: 'https://i.postimg.cc/qMr13CNd/encanto-big.jpg',
+      countOpened: 12,
+      genres: [
+        {
+          _id: '1',
+          description: '',
+          icon: 'Md10K',
+          name: '',
+          slug: '',
+        },
+      ],
+      poster: 'https://i.postimg.cc/qMr13CNd/encanto-big.jpg',
+      rating: 2,
+      slug: 'wq',
+      title: '',
+      videoUrl: '',
+    },
+  ],
+  actors: [],
 }
 
 // Global props context
-export const GlobalPropsContext = createContext<GlobalProps>(defaultGlobalPropsContextValue)
+export const GlobalPropsContext = createContext<GlobalProps>(
+  defaultGlobalPropsContextValue
+)
 
 // Global props context provider props
 export interface GlobalPropsContextProviderProps {
-    children?: ReactNode,
-    globalProps: GlobalProps;
+  children?: ReactNode
+  globalProps: GlobalProps
 }
 
 // Global props context provider
-export function GlobalPropsContextProvider(props: GlobalPropsContextProviderProps) {
-    return <GlobalPropsContext.Provider value={props.globalProps}>
-        {props.children}
+export function GlobalPropsContextProvider(
+  props: GlobalPropsContextProviderProps
+) {
+  return (
+    <GlobalPropsContext.Provider value={props.globalProps}>
+      {props.children}
     </GlobalPropsContext.Provider>
-
+  )
 }
 
 // Utility hook to access global props
 export function useGlobalProps() {
-    return useContext(GlobalPropsContext)
+  return useContext(GlobalPropsContext)
 }
