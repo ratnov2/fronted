9df1525b8fca4f492c7c170624d0aa8306c03f83
ratnov2 <73044570+ratnov2@users.noreply.github.com>
2023-07-13 19:48:36 +0300

    vercel6

--- app/ui/slider/Slider.tsx
+++ app/ui/slider/Slider.tsx
@@ -3,42 +3,42 @@ import MaterialIcon from 'ui/MaterialIcon'
 import style from './slider.module.scss'
 import { CSSTransition } from 'react-transition-group'
 import SliderItem from './slider-item/SliderItem'
-import { useSliderEffect } from './useSliderEffect'
+import useSliderEffect from './useSliderEffect'
 
 const Slider = () => {
-  const { handleClick, popularMoviesConverted, currentIdx, slideIn } =
-    useSliderEffect()
-
+  const { handleClick, popularMoviesConverted,currentIdx, slideIn } = useSliderEffect()
   const nodeRef = useRef(null)
 
   return (
-    
     <div className={style.slider}>
-      {popularMoviesConverted !== undefined ?
-      <>
-      <CSSTransition
-        nodeRef={nodeRef}
-        in={slideIn}
-        timeout={300}
-        classNames="slide-animation"
-      >
-        <div ref={nodeRef}>
-          <SliderItem
-            currentImg={popularMoviesConverted[currentIdx].bigPoster}
-            movie={popularMoviesConverted[currentIdx]}
-          />
-        </div>
-      </CSSTransition>
-      <div className={style.buttonGroup}>
-        <button onClick={() => handleClick('left')}>
-          <MaterialIcon name="MdArrowBackIos" />
-        </button>
-        <button onClick={() => handleClick('right')}>
-          <MaterialIcon name="MdArrowForwardIos" />
-        </button>
-      </div> </>:<div>loading</div>}
+      {popularMoviesConverted !== undefined ? (
+        <>
+          <CSSTransition
+            nodeRef={nodeRef}
+            in={slideIn}
+            timeout={300}
+            classNames="slide-animation"
+          >
+            <div ref={nodeRef}>
+              <SliderItem
+                currentImg={popularMoviesConverted[currentIdx].bigPoster}
+                movie={popularMoviesConverted[currentIdx]}
+              />
+            </div>
+          </CSSTransition>
+          <div className={style.buttonGroup}>
+            <button onClick={() => handleClick('left')}>
+              <MaterialIcon name="MdArrowBackIos" />
+            </button>
+            <button onClick={() => handleClick('right')}>
+              <MaterialIcon name="MdArrowForwardIos" />
+            </button>
+          </div>{' '}
+        </>
+      ) : (
+        <div>loading</div>
+      )}
     </div>
-
   )
 }
 
--- app/ui/slider/useSliderEffect.ts
+++ app/ui/slider/useSliderEffect.ts
@@ -1,23 +1,43 @@
 import { movieApi } from '@/api/dataAPI'
+import { IGenre, IMoviePopular } from '@/shared/types/movie.types'
 import { useGlobalProps } from 'global-props/contexts/GlobalPropsContext'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useQuery } from 'react-query'
 
-export const useSliderEffect = () => {
+const useSliderEffect = () => {
   const [currentIdx, setCurrentIdx] = useState(0)
-  const { popularMovies } = useGlobalProps()
-  //const popularMovies = useQuery('getPopularMovies',()=>movieApi.mostPopular())
+  // const { popularMovies } = useGlobalProps()
+  const [popularMoviesConverted, setMovie] = useState<
+    | { id: string; bigPoster: string; genres: IGenre[]; title: string }[]
+    | undefined
+  >()
+  const popularMovies = useQuery('getPopularMovies', () =>
+    movieApi.mostPopular()
+  )
   const [slideIn, setSlideIn] = useState(false)
-
-  const popularMoviesConverted =
-    popularMovies !== undefined
-      ? popularMovies.map((el) => ({
-          id: el._id,
-          bigPoster: el.bigPoster,
-          genres: el.genres,
-          title: el.title,
-        }))
-      : []
+  useEffect(() => {
+    if (popularMovies.data?.data.length !== 0) {
+      const popularMovie =
+        popularMovies.data?.data !== undefined
+          ? popularMovies.data.data.map((el) => ({
+              id: el._id,
+              bigPoster: el.bigPoster,
+              genres: el.genres,
+              title: el.title,
+            }))
+          : undefined
+          setMovie(popularMovie)
+    }
+  }, [popularMovies.data])
+  // const popularMoviesConverted =
+  //   popularMovies.data?.data !== undefined
+  //     ? popularMovies.data.data.map((el) => ({
+  //         id: el._id,
+  //         bigPoster: el.bigPoster,
+  //         genres: el.genres,
+  //         title: el.title,
+  //       }))
+  //     : []
   const nextIndex = () => {
     if (currentIdx === 2) return setCurrentIdx(0)
     return setCurrentIdx(currentIdx + 1)
@@ -43,3 +63,4 @@ export const useSliderEffect = () => {
     slideIn,
   }
 }
+export default useSliderEffect
