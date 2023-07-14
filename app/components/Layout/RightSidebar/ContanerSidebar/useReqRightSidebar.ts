18c9e8018af41f7c20f2515016692b3452dc2033
ratnov2 <73044570+ratnov2@users.noreply.github.com>
2023-07-15 00:04:34 +0300

    return

--- app/components/Layout/RightSidebar/ContanerSidebar/useReqRightSidebar.ts
+++ app/components/Layout/RightSidebar/ContanerSidebar/useReqRightSidebar.ts
@@ -3,16 +3,15 @@ import { useQuery } from "react-query"
 
 
 export const useReqRightSidebar = ()=>{
-  const popularMovies = {}
-  // useQuery('getPopularMovies', ()=>movieApi.mostPopular(),{
-  //   select:({data}) =>data.slice(0,3).map((el)=>({
-  //     id:el._id,
-  //     name:el.title,
-  //     genres:String(...el.genres.map((el2)=>el2.name)),
-  //     rating:el.rating,
-  //     poster:el.poster,
-  //   }))
-  // })
+  const popularMovies = useQuery('getPopularMovies', ()=>movieApi.mostPopular(),{
+    select:({data}) =>data.slice(0,3).map((el)=>({
+      id:el._id,
+      name:el.title,
+      genres:String(...el.genres.map((el2)=>el2.name)),
+      rating:el.rating,
+      poster:el.poster,
+    }))
+  })
 
   const favorites = useQuery('getFavoritesMovies',()=>usersApi.getFavoriteFilms(),{
     select:({data}) =>data.slice(0,3).map((el)=>({
