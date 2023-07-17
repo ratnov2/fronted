44e7ba79e22759f9850f337fb7c466523802c41e
ratnov2 <73044570+ratnov2@users.noreply.github.com>
2023-07-17 12:48:04 +0300

    9

--- app/components/Layout/Layout.tsx
+++ app/components/Layout/Layout.tsx
@@ -11,7 +11,7 @@ const Layout: FC<{ children: React.ReactNode }> = ({ children }) => {
       <LeftSidebar />
       <div className={style.center}>{children}</div>
       <div></div>
-      {/* <RightSidebar /> */}
+      <RightSidebar />
     </div>
   )
 }
