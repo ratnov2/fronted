91db155c91d150e2601e22e764e645c925fbd811
ratnov2 <73044570+ratnov2@users.noreply.github.com>
2023-07-17 16:09:36 +0300

    add url config

--- app/configs/url.config.ts
+++ app/configs/url.config.ts
@@ -1,4 +1,4 @@
-export const getMovieUrl = (url: string) => `/movie/${url}`
+export const getMovieUrl = (url: string) => `movie/${url}`
 export const getMoviesUrl = (url:string) =>  `/movies/${url}`
 export const getGenreUrl = (slug: string) => `/genre/${slug}`
 export const getActorUrl = (slug: string) => `/actor/${slug}`
