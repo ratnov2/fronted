c96d984cebaeddfab428c6432a4fefedfe021a20
ratnov2 <73044570+ratnov2@users.noreply.github.com>
2023-07-17 12:16:36 +0300

    2

--- app/components/Layout/RightSidebar/ContanerSidebar/FavoritesMovies.tsx
+++ app/components/Layout/RightSidebar/ContanerSidebar/FavoritesMovies.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import SkeletonLoader from 'ui/skeleton-loader/SkeletonLoader'
-
+import style from '../RightSidebar.module.scss'
 import Image from 'next/image'
 import Rating from 'ui/rating/Rating'
 import Button from 'ui/form-ui/button/Button'
@@ -21,22 +21,21 @@ const FavoriteMovies = () => {
             <Link href={`/movie/${el._id}`} className={style.item} key={el._id}>
               <Image width={60} height={150} alt="" src={el.poster} />
               <div>
-                  <span>
-                    <h3>{el.title}</h3>
-                    <div className={style.genres}>
-                      {el.genres.map((el) => (
-                        <span>{el.name}</span>
-                      ))}
-                    </div>
-                  </span>
-                  <Rating rating={el.rating} />
-                </div>
+                <span>
+                  <h3>{el.title}</h3>
+                  <div className={style.genres}>
+                    {el.genres.map((el) => (
+                      <span>{el.name}</span>
+                    ))}
+                  </div>
+                </span>
+                <Rating rating={el.rating} />
+              </div>
             </Link>
-            
           )
         })
       )}
-      <Button className='mx-5 py-2 w-44' >See more</Button>
+      <Button className="mx-5 py-2 w-44">See more</Button>
     </div>
   )
 }
